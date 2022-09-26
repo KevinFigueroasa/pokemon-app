13d5adfa022b1b82707b20b322ec8ff9eceddace
KevinFigueroasa <kevin.figueroasa@gmail.com>
2022-09-26 15:40:23 -0500

    imagen del login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -21,7 +21,7 @@ const Login = () => {
         <div className='login'>
             <div className='welcome-trainer'>
                 <h2>Hi, trainer! <br/>You're ready?</h2>
-                <img id='poke-master' src='src\assets\images\entrenador-pok.png' alt="" />
+                <img id='poke-master' src='https://images.wikidexcdn.net/mwuploads/esssbwiki/thumb/1/1a/latest/20181107165948/Entrenador_Pok%C3%A9mon_SSBU.png/1200px-Entrenador_Pok%C3%A9mon_SSBU.png' alt="" />
             </div>
             {/* <h2>Enter your name to start</h2> */}
             <div className='login-container'>
