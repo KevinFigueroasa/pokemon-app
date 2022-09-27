1a2ba1ffa4e33e65862b7cf34f430fae5aeba48a
KevinFigueroasa <kevin.figueroasa@gmail.com>
2022-09-27 17:18:44 -0500

    mejorando pokemon cards

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import colors from '../../public/colors.json';
 import types from '../../public/types.json'
+import '../../src/css/progress-bar.css'
 
 const PokemonCard = ({url}) => {
 
@@ -34,16 +35,49 @@ const PokemonCard = ({url}) => {
         <div className='pokemon-cards' style={{background: colorsFont()}}>
             <div className='pokemon-data'
             onClick={() => navigate(`/pokemons/${getPokemon.id}`)}>
-                <img src={getPokemon.sprites?.other.dream_world.front_default === null ?
-                    getPokemon.sprites?.other["official-artwork"].front_default : getPokemon.sprites?.other.dream_world.front_default
+                <img src={getPokemon.sprites?.other.dream_world.front_default !== null ?
+                    getPokemon.sprites?.other.dream_world.front_default : getPokemon.sprites?.other["official-artwork"].front_default !== null ? getPokemon.sprites?.other["official-artwork"].front_default : getPokemon.sprites?.other.home.front_default 
                 } alt="" />
                 <div className='back-white'>
                     <h2>{getPokemon.name}</h2>
                     <h4>Type: {getPokemon.types?.[0].type.name}</h4>
                     <div className='principal-pokeinfo'>
-                        <p>HP<br />{getPokemon.stats?.[0].base_stat}</p>
-                        <p>Attack<br />{getPokemon.stats?.[1].base_stat}</p>
-                        <p>Defense<br />{getPokemon.stats?.[2].base_stat}</p>
+                        {/* <p className='values'>{getPokemon.stats?.[0].base_stat}<p className='stats'>HP</p></p>
+                        <p className='values'>{getPokemon.stats?.[1].base_stat}<p className='stats'>Attack</p></p>
+                        <p className='values'>{getPokemon.stats?.[2].base_stat}<p className='stats'>Defense</p></p> */}
+                        <div className='principal-stats'>
+                            <p className='values'>{getPokemon.stats?.[0].base_stat}</p>
+                            <p className='stats'>HP</p>
+                        </div>
+                        <div className='principal-stats'>
+                            <p className='values'>{getPokemon.weight}</p>
+                            <p className='stats'>Weight</p>
+                        </div>
+                        <div className='principal-stats'>
+                            <p>{getPokemon.height}</p>
+                            <p className='stats'>Height</p>
+                        </div>
+                    </div>
+
+                    <div className="abilities">
+                        <div className="abilities__item">
+                            <div className='abilities__item--container'>
+                                <p className="abilities__speed">Attack: {getPokemon.stats?.[1].base_stat}</p>
+                            </div>
+                            <div id='toggle-colors' className={`abilities__bar abilities__bar--${getPokemon.stats?.[1].base_stat <= 30 ? "lent" : getPokemon.stats?.[1].base_stat >=125 ? "rapid" : getPokemon.stats?.[1].base_stat <= 30 ? "lent" : getPokemon.stats?.[1].base_stat}`}></div>
+                        </div>
+                        <div className="abilities__item">
+                            <div className='abilities__item--container'>
+                                <p className="abilities__speed">Defense: {getPokemon.stats?.[2].base_stat}</p>
+                            </div>
+                            <div className={`abilities__bar abilities__bar--${getPokemon.stats?.[2].base_stat <= 30 ? "lent" : getPokemon.stats?.[2].base_stat >=125 ? "rapid" : getPokemon.stats?.[2].base_stat <= 30 ? "lent" : getPokemon.stats?.[2].base_stat}`}></div>
+                        </div>
+                        <div className="abilities__item">
+                            <div className='abilities__item--container'>
+                                <p className="abilities__speed">Speed: {getPokemon.stats?.[5].base_stat}</p>
+                            </div>
+                            <div id='toggle-colors' className={`abilities__bar abilities__bar--${getPokemon.stats?.[5].base_stat <= 30 ? "lent" : getPokemon.stats?.[5].base_stat >=125 ? "rapid" : getPokemon.stats?.[5].base_stat <= 30 ? "lent" : getPokemon.stats?.[5].base_stat}`}></div>
+                        </div>
                     </div>
                 </div>
             </div>
